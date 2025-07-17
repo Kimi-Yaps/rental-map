9e21ae28a98c0a47e31c140808134e93b0789d54
Kimi-Yaps <hakimiiman342ab@gmail.com>
2025-07-17 08:57:57 +0800

    test

---
+++ android/app/src/main/java/rental/map/MainActivity.java
@@ -0,0 +1,5 @@
+package rental.map;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'rental.map',
+  appName: 'rental-map',
+  webDir: 'dist'
+};
+
+export default config;
